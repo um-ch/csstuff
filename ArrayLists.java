651457ff58d9b29ed7f901d2e1b910cbb75a8cb1
um-ch <101957167+um-ch@users.noreply.github.com>
2022-03-27 18:47:16 -0400

    Add files via upload

---
+++ ArrayLists.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class ArrayLists { 
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Contents of array list displayed: ");
+		
+			ArrayList<String> favFoods = new ArrayList<String>(); {
+			
+			favFoods.add("Popcorn");
+			favFoods.add("Chicken");
+			favFoods.add("Icecream");
+			favFoods.add("Pizza");
+			favFoods.add("Hamburgers"); 
+	
+			for (int i = 0; i < favFoods.size(); i++) {
+				System.out.println(favFoods.get(i));
+			}
+		}
+	}
+}
+
