513bd97420b11ffe83beedc957653edd62ba279d
um-ch <101957167+um-ch@users.noreply.github.com>
2022-04-18 14:02:03 -0400

    Add files via upload

---
+++ Polymorphism/Car.java
@@ -0,0 +1,20 @@
+
+public class Car {
+	
+	public void carSound() {
+		System.out.println("VROOM");
+	}
+
+public static void main(String[] args) {
+	
+	var ford = new Ford();
+	
+		ford.carSound();
+	
+}
+		
+		
+		
+
+
+}
---
+++ Polymorphism/Ford.java
@@ -0,0 +1,8 @@
+public class Ford extends Car{
+		
+		public void carSound() {
+			
+			super.carSound();
+			System.out.println("ZOOM");
+		}
+	}	
